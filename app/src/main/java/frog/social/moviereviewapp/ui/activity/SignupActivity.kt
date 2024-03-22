dc061fec3cfba0f64e07a9b3bdd695c75d0d6be5
muhib349 <muhibgazi@gmail.com>
2024-03-22 19:42:39 +0000

    Initial commit

---
+++ app/src/main/java/frog/social/moviereviewapp/ui/activity/SignupActivity.kt
@@ -0,0 +1,12 @@
+package frog.social.moviereviewapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import frog.social.moviereviewapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
---
+++ app/src/main/java/frog/social/moviereviewapp/ui/activity/SplashActivity.kt
@@ -0,0 +1,29 @@
+package frog.social.moviereviewapp.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import frog.social.moviereviewapp.R
+
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DELAY_TIME: Long = 2000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            { navigateToNextScreen() },
+            SPLASH_DELAY_TIME
+        )
+    }
+
+    private fun navigateToNextScreen() {
+        val intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
